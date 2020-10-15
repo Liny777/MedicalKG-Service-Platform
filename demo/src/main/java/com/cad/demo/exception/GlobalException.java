d0006f3854913ef83b969eb3478b533b3f768546
cecila <740795142@qq.com>
2020-10-16 01:25:56 +0800

    Init

---
+++ demo/src/main/java/com/cad/demo/exception/GlobalException.java
@@ -0,0 +1,12 @@
+package com.cad.demo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GlobalException extends RuntimeException {
+    private Integer code;
+    private String msg;
+
+}
